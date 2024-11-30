7f052ee1b2c61e0898207718e5a19cf93776fa02
Ana Carolina <anachan.01h@gmail.com>
2024-11-30 17:28:25 -0300

    Feat[ArrayStack]: implements stack_free function

--- stack/array_stack/main.asm
+++ stack/array_stack/main.asm
@@ -1,4 +1,4 @@
-extern malloc
+extern malloc, free
     
 struc ArrayStack
     .data resq 1
@@ -15,7 +15,14 @@ global main
 main:
     mov rdi, stack
     call stack_init
-    mov rdi, rax
+    test rax, rax
+    jz .exit
+
+    mov rdi, stack
+    call stack_free
+
+.exit:
+    mov rdi, [stack + ArrayStack.capacity]
     mov rax, 0x3C
     syscall
 
@@ -32,12 +39,31 @@ stack_init:
     test rax, rax
     jz .exit
 
-    mov rdx, [rsp]
-    mov [rdx + ArrayStack.data], rax
-    mov [rdx + ArrayStack.capacity], dword 1
-    mov [rdx + ArrayStack.size], dword 0
+    mov rdi, [rsp]
+    mov [rdi + ArrayStack.data], rax
+    mov [rdi + ArrayStack.capacity], dword 1
+    mov [rdi + ArrayStack.size], dword 0
 
     mov rax, 1
 .exit:
+    leave
+    ret
+
+; stack_free(stack: &ArrayStack)
+stack_free:
+    ; # Stack frame
+    ; [rsp]: &ArrayStack
+    enter 8, 0
+    mov [rsp], rdi
+
+    mov rdi, [rdi + ArrayStack.data]
+    call free
+
+    mov rdi, [rsp]
+    xor rax, rax
+    mov [rdi + ArrayStack.data], rax
+    mov [rdi + ArrayStack.capacity], eax
+    mov [rdi + ArrayStack.size], eax
+
     leave
     ret
