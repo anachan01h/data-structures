4d4362f36c26db6a77f72fe981f076bfe1e4ce09
Ana Carolina <anachan.01h@gmail.com>
2024-12-01 00:26:20 -0300

    Feat[ArrayStack]: implements stack_resize function

--- stack/array_stack/main.asm
+++ stack/array_stack/main.asm
@@ -1,4 +1,4 @@
-extern malloc, free
+extern malloc, realloc, free
     
 struc ArrayStack
     .data resq 1
@@ -21,11 +21,21 @@ main:
     test rax, rax
     jz .exit
 
+    mov [stack + ArrayStack.size], dword 1
+    mov rdi, stack
+    call stack_resize
+    test rax, rax
+    jz .exit
+
+    push qword [stack + ArrayStack.capacity]
+    push rax
+
     mov rdi, stack
     call stack_free
 
 .exit:
-    mov rax, [stack + ArrayStack.capacity]
+    pop rax
+    pop rax
     leave
     ret
 
@@ -72,5 +82,36 @@ stack_free:
     mov [rdi + ArrayStack.capacity], eax
     mov [rdi + ArrayStack.size], eax
 
+    leave
+    ret
+
+; stack_resize(stack: &ArrayStack) -> Option<()>
+stack_resize:
+    ; # Stack Frame
+    ; [rbp - 8]: &ArrayStack
+    ; [rbp -12]: u32
+    push rbp
+    mov rbp, rsp
+    sub rsp, 16
+    mov [rbp - 8], rdi          ; save stack as local variable
+
+    mov edx, 1
+    mov esi, [rdi + ArrayStack.size]
+    shl esi, 1                  ; esi <- 2 * size
+    cmovz esi, edx              ; if 2 * size == 0, then esi <- 1
+    mov [rbp - 12], esi         ; save new capacity as local variable
+    mov rdi, [rdi + ArrayStack.data]
+    call realloc
+
+    test rax, rax
+    jz .exit                    ; if error, return None
+
+    mov rdi, [rbp - 8]
+    mov [rdi + ArrayStack.data], rax
+    mov esi, [rbp - 12]
+    mov [rdi + ArrayStack.capacity], esi
+
+    mov eax, 1                  ; return Some(())
+.exit:
     leave
     ret
